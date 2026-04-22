8cf0fba7a5c645930813b8998aaede7794e48e55
tangentbandle3010 <tangentbandle3010@gmail.com>
2026-04-23 01:24:53 +0900

    多分もう使わない

--- test.cpp
+++ test.cpp
@@ -17,14 +17,13 @@ using namespace std;
 using ll = long long;
 
 int main() {
-  int N,K;
-  cin >> N >> K;
-  int ans = 0;
-  for (int i = 0; i < N; ++i) {
-    for (int j = 0; j < K; ++j) {
-      ans += (i+1)*100 + (j+1);
-    }
+  int A, B;
+  cin >> A >> B;
+  if(B % A == 0){
+    cout << A + B << "\n";
   }
-  cout << ans << "\n";
+  else{
+    cout << B - A << "\n";
+  }
   return 0;
 }
