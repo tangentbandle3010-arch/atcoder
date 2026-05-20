b2213230297e29807753483fd54e507bf7f3d863
tangentbandle3010 <tangentbandle3010@gmail.com>
2026-05-20 10:01:43 +0900

    20門といた。　二分探索をちょっと頑張った。

--- test.cpp
+++ test.cpp
@@ -17,13 +17,46 @@ using namespace std;
 using ll = long long;
 
 int main() {
-  int A, B;
-  cin >> A >> B;
-  if(B % A == 0){
-    cout << A + B << "\n";
+  string S;
+  cin >> S;
+  int a = S.size();
+  if(a < 3){
+    cout << 0 << "\n";
+    return 0;
   }
-  else{
-    cout << B - A << "\n";
+  double sum = 0;
+  double min = 0;
+  bool hantei = false;
+  double lef = 0;
+  double rig = 1;
+  double mid = (lef+rig)/2;
+  while(rig-lef>1e-12){
+    for (int i = 0; i < a; ++i) {
+      if(S[i] == 't'){
+        sum += 1-mid;
+      }
+      else{
+        sum += -mid;
+      }
+      if(sum-min >= 2-2*mid){
+        hantei = true;
+        break;
+      }
+      if(min > sum){
+        min = sum;
+      }
+    }
+    if(hantei){
+      lef = mid;
+    }
+    else{
+      rig = mid;
+    }
+    sum = 0;
+    min = 0;
+    hantei = false;
+    mid = (lef+rig)/2;
   }
+  cout << fixed << setprecision(12) << mid << "\n";
   return 0;
 }
